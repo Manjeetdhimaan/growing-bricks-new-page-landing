b3c31733cb73017d18d5b72e015c80df41262bfa
Manjeetdhimaan <manjeetdhimaan80@gmail.com>
2023-02-17 17:18:06 +0530

    updated header

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,22 @@
-function myFunction() {
-    var element = document.getElementById("side-menu");
-    element.classList.toggle("open");
-  }
+(function () {
+
+	"use strict";
+
+	//sticky header on scroll up after scrolling down
+	let lastScroll = 0;
+	let isScrolled = false;
+	window.addEventListener("scroll", function () {
+		let topHeader = document.querySelector(".topheader");
+		let currentScroll =
+			window.pageYOffset ||
+			document.documentElement.scrollTop ||
+			document.body.scrollTop ||
+			0;
+		// var scrollDirection = currentScroll < lastScroll;
+		let scrollDirection = currentScroll;
+		let shouldToggle = isScrolled && scrollDirection;
+		isScrolled = currentScroll > 100;
+		topHeader.classList.toggle("active-topheader", shouldToggle);
+		lastScroll = currentScroll;
+	});
+})()
